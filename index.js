13005681b4eb0579e3451a1169de11f77823b9f0
Adi Bhati <aditya.bhati2001@gmail.com>
2021-11-03 18:08:06 -0400

    first test

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 #!/usr/local/bin/node
 
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 
-console.log('Hello World: ' + uuidv4())
+console.log("Hello World from adi: " + uuidv4());
